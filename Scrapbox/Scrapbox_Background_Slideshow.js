629ba52308dbd057b52086d9a83788a16bb3cb8f
Masamitsu Atarashi <m-atarashi@sumilab.org>
2022-04-16 18:52:44 +0900

    Create Scrapbox_Background_Slideshow.js

---
+++ Scrapbox/Scrapbox_Background_Slideshow.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name         Scrapbox Backgound Slideshow
+// @namespace    https://gist.github.com/m-atarashi/
+// @version      1.0
+// @description  An implementation of background image slideshow on scrapbox.io.
+// @author       m-atarashi
+// @match        https://scrapbox.io/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=scrapbox.io
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // image files of Twiter, Gyazo, Google Photo and etc. are available. Pixiv is not so.
+    const BG_URLS = [
+        {"url": "https://sample.com/blahblahblah1.jpg", "description": "sample image1"},
+        {"url": "https://sample.com/blahblahblah2.jpg", "description": "sample image2"},
+        {"url": "https://sample.com/blahblahblah3.jpg", "description": "sample image3"},
+    ];
+    // interval of slideshow (ms)
+    const BG_INTERVAL = 60000;
+
+    const get_bg_url_rand = (bg_urls) => {
+        const i = Math.floor(Math.random() * bg_urls.length);
+        return bg_urls[i].url;
+    }
+    const change_bg = url => document.querySelector(`:root`).style.setProperty(`--stylus-bg-url`, `url(${url})`);
+
+    setInterval(() => {
+        change_bg(get_bg_url_rand(BG_URLS));
+    }, BG_INTERVAL);
+})();
+
+
+// GyazoのコレクションのBG_URLSを出力する関数
+// コレクションのページを開き、ブラウザのコンソールで実行する
+/*
+(function() {
+const url_stems = Array.from(document.querySelector(".images-grid-view").getElementsByClassName("view-operation")).map(e => `https://i.gyazo.com${e.attributes.href.textContent}`);
+const exts = Array.from(document.querySelector(".images-grid-view").getElementsByClassName("thumb loaded")).map(e => e.attributes.src.textContent.slice(-7,-4));
+const urls = url_stems.map((e, i) =>`${e}.${exts[i]}`);
+const titles = Array.from(document.querySelector(".images-grid-view").getElementsByClassName("metadata")).map(e => e.innerHTML);
+const bgs = urls.map((e, i) => `{"url": "${e}", "description": "${titles[i]}"}`);
+console.log(bgs);
+})()
+*/
