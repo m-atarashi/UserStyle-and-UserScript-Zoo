ef57a90e6f8e846a9d0147ff4c012c1844418f0d
Masamitsu Atarashi <m-atarashi@sumilab.org>
2022-04-19 00:27:47 +0900

    Update redirectTossofunjpac.js

--- HOPE/redirectTossofunjpac.js
+++ HOPE/redirectTossofunjpac.js
@@ -4,13 +4,15 @@
 // @version      0.1
 // @description  redirect to sso.fun.ac.jp automatically
 // @author       m-atarashi
-// @match        https://hope.fun.ac.jp/
+// @match        https://hope.fun.ac.jp/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=fun.ac.jp
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-
-    location.href = "https://hope.fun.ac.jp/auth/saml2/login.php?idp=1bec319bca7458548c77d545a2a1b3de";
+    const regex = /^https:\/\/hope\.fun\.ac\.jp\/(local\/hope\/login.php.*)?$/;
+    if(regex.test(location.href)){
+        location.href = "https://hope.fun.ac.jp/auth/saml2/login.php?idp=1bec319bca7458548c77d545a2a1b3de";
+    }
 })();
