9aaaa0378e090dd751b40653e72f8e26b3659a91
Masamitsu Atarashi <m-atarashi@sumilab.org>
2022-04-18 22:08:53 +0900

    Create autoLoginTossofunacjp.js

---
+++ HOPE/autoLoginTossofunacjp.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Auto Login sso.fun.ac.jp
+// @namespace    https://github.com/m-atarashi/UserStyle-Zoo/new/main/HOPE
+// @version      0.1
+// @description  auto login sso.fun.ac.jp
+// @author       m-atarashi
+// @match        https://sso.fun.ac.jp/my.policy
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=fun.ac.jp
+// @grant        none
+// ==/UserScript==
+
+(function() {
+   setTimeout(() => document.querySelector(`#auth_form`).submit(), 1000);
+})();
