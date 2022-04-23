09406bfa96533479f6bb18c9a5f3ef8c5db77509
Masamitsu Atarashi <m-atarashi@sumilab.org>
2022-04-23 19:41:17 +0900

    fix: make eable to load images asynchronously

--- Scrapbox/Background_Slideshow.js
+++ Scrapbox/Background_Slideshow.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Scrapbox Backgound Slideshow
 // @namespace    https://github.com/m-atarashi/UserStyle-Zoo/tree/main/Scrapbox
-// @version      1.0
+// @version      1.1
 // @description  An implementation of background image slideshow on scrapbox.io.
 // @author       m-atarashi
 // @match        https://scrapbox.io/*
@@ -14,22 +14,25 @@
 
     // image files of Twiter, Gyazo, Google Photo and etc. are available. Pixiv is not so.
     const BG_URLS = [
-        {"url": "https://sample.com/blahblahblah1.jpg", "description": "sample image1"},
-        {"url": "https://sample.com/blahblahblah2.jpg", "description": "sample image2"},
-        {"url": "https://sample.com/blahblahblah3.jpg", "description": "sample image3"},
+        {"description": "sample hoge", "url": "https://sample.com/image1.jpg"},
+        {"description": "sample hoge", "url": "https://sample.com/image2.jpg"},
+        {"description": "sample hoge", "url": "https://sample.com/image3.jpg"},
     ];
+    
     // interval of slideshow (ms)
-    const BG_INTERVAL = 60000;
+    const BG_INTERVAL = 60000
 
     const get_bg_url_rand = (bg_urls) => {
-        const i = Math.floor(Math.random() * bg_urls.length);
-        return bg_urls[i].url;
+        const i = Math.floor(Math.random() * bg_urls.length)
+        return bg_urls[i].url
     }
-    const change_bg = url => document.querySelector(`:root`).style.setProperty(`--stylus-bg-url`, `url(${url})`);
+    const change_bg = url => document.querySelector(`:root`).style.setProperty(`--stylus-bg-url`, `url(${url})`)
 
+    let img = new Image()
     setInterval(() => {
-        change_bg(get_bg_url_rand(BG_URLS));
-    }, BG_INTERVAL);
+        change_bg(img.src)
+        img.src = get_bg_url_rand(BG_URLS)
+    }, BG_INTERVAL)
 })();
 
 
